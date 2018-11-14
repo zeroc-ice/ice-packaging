9f76e8759b692ef60562705f54ba3247974bffdb
Jose <jose@zeroc.com>
2018-11-14 10:17:47 +0100

    Update version to 3.7.2

--- ice/pypi/setup.py
+++ ice/pypi/setup.py
@@ -168,7 +168,7 @@ with open('README.rst') as file:
 setup(
     name='zeroc-ice',
 
-    version='3.7.1',
+    version='3.7.2',
 
     description="Ice is a comprehensive RPC framework with support for Python, C++, .NET, Java, JavaScript and more.",
 
