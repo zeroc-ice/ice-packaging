f9bd3c60d198abdea41c1dee2868b0fa69c04d53
Joe George <joe@zeroc.com>
2021-02-15 09:52:44 -0500

    Update version to v3.8.0

--- ice/pypi/setup.py
+++ ice/pypi/setup.py
@@ -183,7 +183,7 @@ class BuildExt(build_ext):
 setup(
     name='zeroc-ice',
 
-    version='3.7.5',
+    version='3.8.0',
 
     description="Ice is a comprehensive RPC framework with support for Python, C++, .NET, Java, JavaScript and more.",
 
